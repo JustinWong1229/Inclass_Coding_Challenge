8e58711d961ad71782cf8da2a660400a98eff4f2
Justin Wong <jcwong5@usf.edu>
2025-09-10 18:01:27 -0400

    Inital Commit

--- cc_3b.js
+++ cc_3b.js
@@ -1,3 +1,9 @@
-// Coding Challenge 00
+const inventory = [
+    {SKU: "SKU-0004", Name: "Pencil", Price: 5.00, Stock: 10},
+    {SKU: "SKU-0002", Name: "Pen", Price: 7.00, Stock: "12" },
+    {SKU: "SKU-0007", Name: "Chair",Price: 15.00, Stock: "5" },
+    {SKU: "SKU-0010", Name: "Marker", Price: 20.00, Stock: "17"}
+]
 
-// Code goes here
+inventory.forEach(obj =>console.log(`SKU: ${obj.SKU}`)(`Name: ${obj.Name}`));
+
